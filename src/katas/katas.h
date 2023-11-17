524e73f789cd07821a0c44cf5b723b3b7f80f79c
ohmushi <omnes.theo@gmail.com>
2023-11-17 13:53:20 +0100

    remove file name in code

--- src/katas/katas.h
+++ src/katas/katas.h
@@ -27,15 +27,4 @@ struct KataListParsingResult {
 
 void free_kata_list(KataList *kata_list);
 
-static const char * kata_folder_path = "../katas/";
-
-static const char * kata_file_names[] = {
-        "success.c",
-        "compilation_error.c",
-        "run_error.c",
-        "test_error.c"
-};
-
-static const int nb_kata_files = sizeof(kata_file_names) / sizeof(kata_file_names[0]);
-
 #endif //CLINGS_KATAS_H

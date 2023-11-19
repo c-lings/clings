07a3fa7da30e84753762cdfb4fb7b4760e9fc608
ohmushi <omnes.theo@gmail.com>
2023-11-19 11:33:11 +0100

    add a test nothing for yaml

---
+++ src/yaml/yaml_test.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+
+
+static void test_nothing(void **state) {
+    (void) state; /* unused */
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+            cmocka_unit_test(test_nothing),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
+

5ce264434909f3278e814b0adff224e313805056
ohmushi <omnes.theo@gmail.com>
2023-11-20 17:19:17 +0100

    add test for parsing yaml

--- src/yaml/yaml_test.c
+++ src/yaml/yaml_test.c
@@ -1,11 +1,17 @@
 
 #include "CUnit/Basic.h"
 #include "yaml_parser_factory.h"
+#include "yml.h"
 
-#define UNUSED(x) (void)(x)
 
 static FILE* temp_file = NULL;
 static char* file_path = "temp.yml";
+
+void clear_file(void) {
+    if(temp_file != NULL) {
+        freopen(file_path, "w+", temp_file);
+    }
+}
 
 int init(void) {
     if (NULL == (temp_file = fopen(file_path, "w+")))
@@ -37,6 +43,29 @@ void test_parser_factory_with_bad_path(void) {
     yaml_parser_delete(&parser);
 }
 
+void test_parse_exercices(void) {
+    clear_file();
+
+    fprintf(temp_file, "exercises:\n"
+                       "  - name: intro\n"
+                       "    path: katas/intro.c\n"
+                       "\n"
+                       "  - name: success\n"
+                       "    path: katas/success.c\n");
+
+    yaml_parser_t parser;
+    yaml_parser_initialize(&parser);
+    yaml_parser_set_input_file(&parser, temp_file);
+    kata_list_parsing_result_t res = parse_kata_list(&parser);
+
+    CU_ASSERT(res.success);
+    CU_ASSERT_EQUAL_FATAL(res.kata_list.len, 2);
+    CU_ASSERT_STRING_EQUAL(res.kata_list.katas[0].name.str, "intro");
+    CU_ASSERT_STRING_EQUAL(res.kata_list.katas[0].path.str, "katas/intro.c");
+    CU_ASSERT_STRING_EQUAL(res.kata_list.katas[1].name.str, "success");
+    CU_ASSERT_STRING_EQUAL(res.kata_list.katas[1].path.str, "katas/success.c");
+}
+
 int main(void) {
     CU_pSuite pSuite = NULL;
 
@@ -52,6 +81,7 @@ int main(void) {
 
     CU_add_test(pSuite, "given file exists", test_parser_factory);
     CU_add_test(pSuite, "given file does not exist", test_parser_factory_with_bad_path);
+    CU_add_test(pSuite, "correct yaml with two exercices", test_parse_exercices);
 
     /* Run all tests using the CUnit Basic interface */
     CU_basic_set_mode(CU_BRM_VERBOSE);
